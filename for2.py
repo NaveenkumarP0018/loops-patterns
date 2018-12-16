0e16150c63cddb28966e9e1f4f45e32929920fe2
NaveenkumarP0018 <44505480+NaveenkumarP0018@users.noreply.github.com>
2018-12-16 14:25:17 +0530

    patterns

---
+++ for2.py
@@ -0,0 +1,385 @@
+for x in range(1,6):
+    for y in range(1,x+1):
+        print("*",end=" ")
+    print("")
+for x in range(4,0,-1):
+    for y in range(1,x+1):
+        print("*",end=" ")
+    print("")
+
+print("===================================")
+
+for x in range(3,4):
+    for y in range(x):
+        print("*",end=" ")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*   ",end="")
+    print(" ")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end=" ")
+    print("")
+for x in range(3,4):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*   ",end="")
+    print(" ")
+for x in range(2,3):
+    for y in range(x):
+        print("*   ",end="")
+    print(" ")
+for x in range(2,3):
+    for y in range(x):
+        print("*   ",end="")
+    print(" ")
+
+print("==============================")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+
+print("===========================")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+print("============================")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+print("=============================")
+for x in range(5,6):
+    for y in range(x):
+        print("*",end=" ")
+    print("")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(3,4):
+    for y in range(x):
+        print("*   ",end="")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(5,6):
+    for y in range(x):
+        print("*",end=" ")
+    print("")
+print("==================================")
+
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(5,6):
+    for y in range(x):
+        print("*",end=" ")
+    print("")
+
+print("========================")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+print("=================")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+
+print("==============================")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+
+for x in range(3,4):
+    for y in range(x):
+        print("*",end="   ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+print("============================")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*  ",end="     ")
+    print("")
+for x in range(3,4):
+    for y in range(x):
+        print("*",end="   ")
+    print("")
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="")
+    print("")
+
+for x in range(1):
+    print("          *")
+
+print("======================")
+
+for x in range(5,6):
+    for y in range(x):
+        print("* ",end="  ")
+    print("")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+for x in range(1):
+    print("        *")
+print("=========================")
+for x in range(3,4):
+    for y in range(x):
+        print("*",end="  ")
+    print("")
+for x in range(2,3):
+    for y in range(2,4):
+        print("*",end="     ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("*",end="  ")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+for x in range(1):
+    print("*")
+
+print("========================================")
+
+for x in range(5,6):
+    for y in range(x):
+        print(">",end=" ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print(">      ",end=" ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print(">    ",end=" ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print(">  ",end=" ")
+    print(" ")
+for x in range(2,3):
+    for y in range(x):
+        print("> ",end=" ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print(">  ",end=" ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print(">   ",end=" ")
+    print(" ")
+for x in range(2,3):
+    for y in range(x):
+        print(">    ",end=" ")
+    print(" ")
+
+
+print("====================================")
+
+for x in range(2,3):
+    for y in range(x):
+        print("x       ",end=" ")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("  x ",end=" ")
+    print(" ")
+for x in range(1):
+    print("     x")
+for x in range(2,3):
+    for y in range(x):
+        print("  x   ",end="")
+    print("")
+for x in range(2,3):
+    for y in range(x):
+        print("x        ",end=" ")
+    print(" ")
+print("=============================")
+for x in range(5,6):
+    for y in range(x):
+        print("z    ",end="")
+    print("")
+for x in range(1):
+    print("                  z",end="")
+print("")
+for x in range(1):
+    print("               z",end="")
+print("")
+for x in range(1):
+    print("           z",end="")
+print("")
+
+for x in range(1):
+    print("       z",end="")
+print("")
+for x in range(1):
+    print("   z",end="")
+print("")
+for x in range(5,6):
+    for y in range(x):
+        print("z    ",end="")
+    print("")
+print("===============================")
